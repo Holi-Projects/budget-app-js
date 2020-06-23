2f768e1aea7ff0a26f6514415d1f95533d4858ba
Holi-Projects <holi414@outlook.com>
2020-06-23 14:20:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# budget-app-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/budget-app-js)
---
+++ index.js
@@ -0,0 +1,90 @@
+
+const bud = document.querySelector("#amount")
+const calc = document.querySelector("#calc")
+const expName = document.querySelector("#exp-name")
+const exp = document.querySelector("#exp")
+const addEx = document.querySelector("#addEx")
+const alertVal = document.querySelector(".alert")
+const exAm = document.querySelector("#exAm")
+let exSum = 0;
+let budget = 0;
+
+
+
+addEx.addEventListener("click", function(){
+
+    if(expName.value == '' || exp.value == ''){
+       alertDisplay()
+    }
+    else {
+    
+    const display = document.querySelector("#display")
+
+    let li = document.createElement("li")
+
+    li.innerHTML = "<h6>" +expName.value+"</h6>" + "<h5>$" + exp.value+"</h5>" + 
+    "<a href='#' class='editItem' onclick='editItem(this)'><i class='fas fa-edit' aria-hidden='true'></i></a>&nbsp;&nbsp;" + 
+    "<a href='#' class='trash' onclick='rm(this)'><i class='fas fa-trash' aria-hidden='true'></i></a>" ;
+     
+    li.classList.add("list-item")
+    display.appendChild(li)
+    
+    exSum += parseInt(exp.value)
+    exAm.innerHTML = exSum
+    document.querySelector("#bal").innerHTML = bal(budget, exSum)
+    expName.value = ''
+    exp.value = ''
+    
+    }
+})
+
+
+calc.addEventListener("click", function(){
+    if(bud.value == ''){
+        alertDisplay()
+    }
+    else{
+        budget = parseInt(bud.value)
+        document.querySelector("#budAm").innerHTML = bud.value
+        document.querySelector("#bal").innerHTML = bal(budget, exSum)
+        bud.value = ''
+    }
+})
+
+function alertDisplay(){
+    alertVal.append("Values can't be empty or negative (:")
+    alertVal.style.display = 'block'
+    setTimeout(() => {
+        alertVal.innerHTML = ""
+        alertVal.style.display = 'none'
+    }, 1000);
+}
+
+function bal(amountIn, amountOut){
+   
+    const balAmount = amountIn - amountOut
+    
+    return balAmount
+}
+
+function rm(btn){
+
+    let amt = btn.parentElement.childNodes[1].innerText
+    exSum = exSum - parseInt(amt.slice(1, amt.length))
+    document.querySelector("#bal").innerHTML = bal(budget, exSum)
+    exAm.innerHTML = exSum
+    btn.parentElement.remove()
+    
+}
+
+function editItem(btn){
+  
+    let name = btn.parentElement.firstChild.innerText
+    let amt = btn.parentElement.childNodes[1].innerText
+    
+    expName.value = name
+    exp.value = parseInt(amt.slice(1, amt.length))
+    rm(btn)
+}
+
+
